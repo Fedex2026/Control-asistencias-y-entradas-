3c524d49cec9b9b10cbb0616c2cf31b5d8a6d607
Fedex2026 <gruasmetro@gmail.com>
2026-08-05 21:23:00 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -10,20 +10,15 @@
   7. Sustituye los datos de ejemplo de abajo.
 */
 
-export const firebaseConfig = {
-  apiKey: "PEGA_AQUI_TU_API_KEY",
-
-  authDomain: "PEGA_AQUI.firebaseapp.com",
-
-  projectId: "PEGA_AQUI",
-
-  storageBucket: "PEGA_AQUI.appspot.com",
-
-  messagingSenderId: "PEGA_AQUI",
-
-  appId: "PEGA_AQUI"
+const firebaseConfig = {
+  apiKey: "AIzaSyAG1UPUKECymTZ3aifFQuyN_mbviYjm2Q4",
+  authDomain: "control-asistencias-y-entradas.firebaseapp.com",
+  databaseURL: "https://control-asistencias-y-entradas-default-rtdb.firebaseio.com",
+  projectId: "control-asistencias-y-entradas",
+  storageBucket: "control-asistencias-y-entradas.firebasestorage.app",
+  messagingSenderId: "420567524268",
+  appId: "1:420567524268:web:5ec9a3e319ec56a6395009"
 };
-
 
 /*
   WEBHOOK OPCIONAL PARA WHATSAPP
